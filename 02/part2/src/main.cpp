989e864719124b59152090785a454d05289d63ed
Ali Bozorgzadeh <aliiiib95@gmail.com>
2021-12-18 10:58:56 +0330

    Add day02/part{1,2}

---
+++ 02/part2/src/main.cpp
@@ -0,0 +1,54 @@
+#include <cstdint>
+#include <iostream>
+#include <stdexcept>
+
+#include "utils.h"
+
+struct Position {
+    uint32_t x;  // horizontal position       (→ +)
+    uint32_t y;  // vertical position / depth (↓ +)
+    uint32_t aim;
+};
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: dive1 INPUTFILE\n";
+        return 1;
+    }
+
+    // Parse the planned course into a vector of pairs
+    // - each pair holds an instruction
+    // - each instruction is comprised of:
+    //   - a direction, and
+    //   - a magnitude
+    std::vector<std::pair<std::string, uint32_t>> course;
+    try {
+        course = parseTwoPerLine<std::string, uint32_t>(argv[1]);
+    } catch (const std::runtime_error &e) {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
+
+    // Initial position
+    Position pos{0, 0, 0};
+    // Navigate
+    for (const auto &instruction : course) {
+        const auto &direction = instruction.first;
+        const auto &magnitude = instruction.second;
+
+        if (direction == "up") {
+            pos.aim -= magnitude;
+        } else if (direction == "down") {
+            pos.aim += magnitude;
+        } else if (direction == "forward") {
+            pos.x += magnitude;
+            pos.y += magnitude * pos.aim;
+        }
+    }
+
+    // Print the result
+    std::cout << "Final Position: (" << pos.x << ", " << pos.y << ")\n";
+    std::cout << "Final Answer: x * y = " << pos.x * pos.y << '\n';
+
+    return 0;
+}
